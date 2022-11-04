2164891ba0dae7e24d88fa227e196b997e19d725
lakshyasuthar <lakshyasuthar.freelance@gmail.com>
2022-11-04 13:03:37 +0530

    second commit

--- server.js
+++ server.js
@@ -5,28 +5,111 @@ const path = require("path");
 const app = express();
 const publicDirectory = path.join(__dirname, "./public");
 const fs = require("fs");
-const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
-const stripePublicKey = process.env.STRIPE_PUBLISHABLE_KEY;
-
+const bcrypt = require("bcrypt"); // Importing bcrypt package
+const passport = require("passport");
+const initializePassport = require("./passport-config");
+const flash = require("express-flash");
+const session = require("express-session");
+const methodOverride = require("method-override");
 app.use(express.static(publicDirectory));
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
+initializePassport(
+  passport,
+  (email) => users.find((user) => user.email === email),
+  (id) => users.find((user) => user.id === id)
+);
+
+const users = [];
+
+app.use(express.urlencoded({ extended: false }));
+app.use(flash());
+app.use(
+  session({
+    secret: process.env.SECRET_KEY,
+    resave: false, // We wont resave the session variable if nothing is changed
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(methodOverride("_method"));
+
+// Configuring the register post functionality
+app.post(
+  "/login",
+  checkNotAuthenticated,
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })
+);
+
+// Configuring the register post functionality
+app.post("/register", checkNotAuthenticated, async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+    console.log(users); // Display newly registered in the console
+    res.redirect("/login");
+  } catch (e) {
+    console.log(e);
+    res.redirect("/register");
+  }
+});
+
+// Routes
+
+app.get("/", checkAuthenticated, (req, res) => {
   fs.readFile("items.json", function (err, data) {
     if (err) {
       res.status(500).end();
     } else {
-      res.render("index", {
-        stripePublicKey: stripePublicKey,
+      res.render("index.ejs", {
         items: JSON.parse(data),
+        name: req.user.name,
       });
     }
   });
 });
+
 app.get("/about", (req, res) => {
   res.render("aboutUs");
 });
 
-app.listen(5000, () => {
-  console.log("Server is running on port 5k");
+app.get("/login", checkNotAuthenticated, (req, res) => {
+  res.render("login.ejs");
 });
+
+app.get("/register", checkNotAuthenticated, (req, res) => {
+  res.render("register.ejs");
+});
+
+app.delete("/logout", (req, res) => {
+  req.logout(req.user, (err) => {
+    if (err) return next(err);
+    res.redirect("/");
+  });
+});
+
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
+}
+
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect("/");
+  }
+  next();
+}
+
+app.listen(5000);
